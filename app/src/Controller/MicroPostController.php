bcd6336f9e4e42f2b30fb01744b7f43b248fb3d6
mvxj <mvxj11@gmail.com>
2022-09-21 19:08:03 +0200

    Add sensio extra bundle, adjust micro posts templates

--- app/src/Controller/MicroPostController.php
+++ app/src/Controller/MicroPostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class MicroPostController extends AbstractController
 {
     #[Route('/micro/post', name: 'app_micro_post')]
-    public function index(): Response
+    public function index(MicroPostRepository $microPostRepository): Response
     {
         return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
+            'posts' => $microPostRepository->findAll(),
+        ]);
+    }
+
+    #[Route('micro/post/{post}', name: 'app_micro_post_show')]
+    public function showOne(MicroPost $post): Response
+    {
+        return $this->render('micro_post/show.html.twig', [
+            'post' => $post,
         ]);
     }
 }
